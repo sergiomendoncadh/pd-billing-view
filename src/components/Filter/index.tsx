1da3eb0146725fb2588e87727d9839232e9ed228
Narkas Vaniurkina <narkas.vaniurkina@deliveryhero.com>
2023-03-03 14:46:44 +0100

    PBP-489: remove the checkbox component

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { FilterLayout } from '@deliveryhero/armor-filter';
 import {
   Button,
-  Stack,
   Dropdown,
   Box
 } from '@deliveryhero/armor';
 import styles from './Filter.module.css';
 import { Text } from '../Text';
 import { SearchInput } from '../SearchInput';
-import { FilterCheckbox } from '../FilterCheckbox';
 
 export const Filter = () => {
   return (
@@ -21,11 +19,6 @@ export const Filter = () => {
       />
       <Box className={styles.filtersContainer}>
         <SearchInput />
-        <Stack className={styles.checkboxFilters}>
-          <FilterCheckbox checkboxLabel='Billable' tooltipInfo='Order is billable' />
-          <FilterCheckbox checkboxLabel='Receiptable' tooltipInfo='Customer receipt is generated for the order' />
-          <FilterCheckbox checkboxLabel='Wastage' tooltipInfo='Order is accepted by the vendor but is cancelled pre-delivery' />
-        </Stack>
         <Box className={styles.statusDropdown}>
           <Dropdown options={['Delivered', 'Paid', 'Cancelled']} label='Status' />
         </Box>
